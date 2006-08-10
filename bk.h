dc0a518004de551b6f6c3a25fbbff7f66cc0b85b
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2006-08-10 22:25:27 +0000

    started work on the add button
    git-svn-id: svn://littlesvr.ca/prog/isomaster/src/bk@87 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- bk.h
+++ bk.h
@@ -182,5 +182,6 @@ typedef struct
 
 int bk_read_vol_info(int image, VolInfo* volInfo);
 int bk_get_dir_from_string(Dir* tree, char* pathStr, Dir** dirFoundPtr);
+int bk_add_dir(Dir* tree, char* srcPathAndName, char* destPathAndName);
 
 #endif
