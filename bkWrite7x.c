6ec0f22a37fbabb1d84fd822a3a7a6786222d5f4
andrei <andrei@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2006-09-09 18:23:38 +0000

    added boot record support for reading and writing, both to the library and the gui. writing no-emulation doesn't seem to work though. weird things don't happen if the current fs directory has been deleted after iso master quit. bkwrite7x functions no longer take a pointer to the data. other stuff i don't remember.
    git-svn-id: svn://littlesvr.ca/prog/isomaster/src/bk@118 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

--- bkWrite7x.c
+++ bkWrite7x.c
@@ -6,47 +6,53 @@
 
 #include <unistd.h>
 
-int write711(int image, unsigned char* value)
+int write711(int image, unsigned char value)
 {
-    return write(image, value, 1);
+    return write(image, &value, 1);
 }
 
-int write712(int image, signed char* value)
+int write712(int image, signed char value)
 {
-    return write(image, value, 1);
+    return write(image, &value, 1);
 }
 
-int write721(int image, unsigned short* value)
+int write721(int image, unsigned short value)
 {
-    return write(image, value, 2);
+    return write(image, &value, 2);
 }
 
-int write722(int image, unsigned short* value)
+void write721ToByteArray(unsigned char* dest, unsigned short value)
+{
+    dest[0] = value & 0xFF;
+    dest[1] = (value >> 8) & 0xFF;
+}
+
+int write722(int image, unsigned short value)
 {
     int rc;
     unsigned short preparedValue;
     
-    preparedValue = *value;
+    preparedValue = value;
     preparedValue <<= 8;
-    preparedValue |= *value >> 8;
+    preparedValue |= value >> 8;
     
     rc = write(image, &preparedValue, 2);
     
     return rc;
 }
 
-int write723(int image, unsigned short* value)
+int write723(int image, unsigned short value)
 {
     int rc;
     short preparedValue;
     
-    rc = write(image, value, 2);
+    rc = write(image, &value, 2);
     if(rc != 2)
         return rc;
     
-    preparedValue = *value;
+    preparedValue = value;
     preparedValue <<= 8;
-    preparedValue |= *value >> 8;
+    preparedValue |= value >> 8;
     
     rc = write(image, &preparedValue, 2);
     if(rc != 2)
@@ -55,51 +61,59 @@ int write723(int image, unsigned short* value)
     return 4;
 }
 
-int write731(int image, unsigned* value)
+int write731(int image, unsigned value)
 {
-    return write(image, value, 4);
+    return write(image, &value, 4);
 }
 
-int write732(int image, unsigned* value)
+void write731ToByteArray(unsigned char* dest, unsigned value)
+{
+    dest[0] = value & 0xFF;
+    dest[1] = (value >> 8) & 0xFF;
+    dest[2] = (value >> 16) & 0xFF;
+    dest[3] = value >> 24;
+}
+
+int write732(int image, unsigned value)
 {
     int rc;
     unsigned preparedValue;
     
-    preparedValue = *value & 0xFF;
+    preparedValue = value & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 8) & 0xFF;
+    preparedValue |= (value >> 8) & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 16) & 0xFF;
+    preparedValue |= (value >> 16) & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 24);
+    preparedValue |= (value >> 24);
     
     rc = write(image, &preparedValue, 4);
     
     return rc;
 }
 
-int write733(int image, unsigned* value)
+int write733(int image, unsigned value)
 {
     int rc;
     unsigned preparedValue;
     
-    rc = write(image, value, 4);
+    rc = write(image, &value, 4);
     if(rc != 4)
         return rc;
     
-    preparedValue = *value & 0xFF;
+    preparedValue = value & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 8) & 0xFF;
+    preparedValue |= (value >> 8) & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 16) & 0xFF;
+    preparedValue |= (value >> 16) & 0xFF;
     preparedValue <<= 8;
     
-    preparedValue |= (*value >> 24);
+    preparedValue |= (value >> 24);
     
     rc = write(image, &preparedValue, 4);
     if(rc != 4)
