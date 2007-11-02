904261b48f0638a76c8bad61cb424a6dfa6ca3e2
andrew <andrew@ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea>
2007-11-02 06:08:34 +0000

    added almost-empty bkIoWrappers.c/.h
    git-svn-id: svn://littlesvr.ca/prog/isomaster/src/bk@488 ea4051ab-32fe-0310-aa8c-8a8c8f7b8cea

---
+++ bkIoWrappers.c
@@ -0,0 +1,22 @@
+/******************************* LICENCE **************************************
+* Any code in this file may be redistributed or modified under the terms of
+* the GNU General Public Licence as published by the Free Software 
+* Foundation; version 2 of the licence.
+****************************** END LICENCE ***********************************/
+
+/******************************************************************************
+* Author:
+* Andrew Smith, http://littlesvr.ca/misc/contactandrew.php
+*
+* Contributors:
+* 
+******************************************************************************/
+
+#include <stdio.h>
+
+#include "bkInternal.h"
+
+bk_off_t bk_lseek_set(VolInfo* volInfo, bk_off_t offset, int origin)
+{
+    
+}
---
+++ bkIoWrappers.h
@@ -0,0 +1,1 @@
+bk_off_t bk_lseek_set(VolInfo* volInfo, bk_off_t offset, int origin);
